0b88c892f6e0ac60498a949003e55878bd31608c
Victor <victor.martins.santos05@escola.pr.gov.br>
2025-09-16 11:14:29 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,9 +1,9 @@
 export const perguntas = [
     {
-        enunciado: "Você prefere nike ou adidas?",  //a pergunta
+        enunciado: "Você prefere doce ou salgado?",  //a pergunta
         alternativas: [
             {
-                texto: "nike", //primeira alternativa
+                texto: "salgado", //primeira alternativa
                 afirmacao: [
                     "você tem bom gosto", //comentário para a resposta
                     "afirmação 2"        //segundo comentário
@@ -11,9 +11,9 @@ export const perguntas = [
                 proxima: 1,
             },
             {
-                texto: "adidas",
+                texto: "doce",
                 afirmacao: [
-                    "você é vaiado",
+                    "não só de doce vive o homem",
                     "você não sabe escolher"
                 ],
                 proxima: 1,
@@ -21,83 +21,83 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "pergunta 2",
+        enunciado: "praia ou fazenda?",
         alternativas: [
             {
-                texto: "nike",
+                texto: "praia",
                 afirmacao: [
-                    "você tem bom gosto",
+                    "gosto até bom",
                     "afirmação 2"
                 ],
                 proxima: 2,
             },
             {
-                texto: "adidas",
+                texto: "voce tem um otimo gosto",
                 afirmacao: [
-                    "você é vaiado",
-                    "você não sabe escolher"
+                    "adimiro suas escolhas",
+                    "tem bom gosto"
                 ],
                 proxima: 2,
             }
         ]
     },
     {
-        enunciado: "pergunta 4",
+        enunciado: "sair ou ficar em casa?",
         alternativas: [
             {
-                texto: "nike",
+                texto: "sair",
                 afirmacao: [
-                    "você tem bom gosto",
+                    "é bom sair de casa pra dar um role",
                     "afirmação 2"
                 ],
                 proxima: 3,
             },
             {
-                texto: "adidas",
+                texto: "ficar em casa",
                 afirmacao: [
-                    "você é vaiado",
-                    "você não sabe escolher"
+                    "você tem um gosto bom por opitar ficar no conforto de casa",
+                    "boa escolha"
                 ],
                 proxima: 3,
             }
         ]
     },
     {
-        enunciado: "Você prefere nike ou adidas?",
+        enunciado: "computador ou console",
         alternativas: [
             {
-                texto: "nike",
+                texto: "computador",
                 afirmacao: [
-                    "você tem bom gosto",
+                    "bom gosto",
                     "afirmação 2"
                 ],
                 proxima: 4,
             },
             {
-                texto: "adidas",
+                texto: "console",
                 afirmacao: [
-                    "você é vaiado",
-                    "você não sabe escolher"
+                    "muito bom, tem um otimo conforto",
+                    "pode jogar tranquilo no sofá kk"
                 ],
                 proxima: 4,
             }
         ]
     },
     {
-        enunciado: "pergunta 5",
+        enunciado: "android ou iphone",
         alternativas: [
             {
-                texto: "nike",
+                texto: "android",
                 afirmacao: [
-                    "você tem bom gosto",
-                    "afirmação 2"
+                    "você tem bom gosto, custo beneficio e bom",
+                    "alguns até superam o iphone"
                 ],
             },
             {
-                texto: "adidas",
+                texto: "iphone",
                 afirmacao: [
-                    "você é vaiado",
-                    "você não sabe escolher"
+                    "voce tem um bom gosto",
+                    "mais tem um custo elevado"
                 ],
             }
         ]
